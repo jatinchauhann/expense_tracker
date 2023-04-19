7bff544304e2176207879d9ab553e3f15fe84768
jatinchauhann <jtnchauhan24@gmail.com>
2023-04-19 23:41:54 +0530

    using Dismissible class to remove items from the data

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -44,6 +44,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +64,8 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+                expenses: _registeredExpenses, onRemoveExpense: _removeExpense),
           )
         ],
       ),
