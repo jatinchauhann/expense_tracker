5def255d9c6a5f025f47852f084c72145d934de4
jatinchauhann <jtnchauhan24@gmail.com>
2023-04-19 22:50:46 +0530

    added showDatePicker and worked with class of type Future

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -12,10 +12,26 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -31,9 +47,42 @@ class _NewExpenseState extends State<NewExpense> {
         ),
         Row(
           children: [
+            Expanded(
+                child: TextField(
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                  prefixText: '\$ ', label: Text('Amount')),
+            )),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(_selectedDate == null
+                      ? 'No Date Selected'
+                      : formatter.format(
+                          _selectedDate!)), // with ! mark we tell dart that it can never be null
+                  IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month))
+                ],
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context); // Removes overlay from the screen
+                },
+                child: const Text('Cancel')),
             ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('Save Expense'))
           ],
