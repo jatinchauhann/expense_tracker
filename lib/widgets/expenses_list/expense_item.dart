b4ff8743024714ac934c93ba991f92a74f07de62
jatinchauhann <jtnchauhan24@gmail.com>
2023-04-21 09:54:14 +0530

    some more visual flai

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -16,20 +16,26 @@ class ExpenseItem extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              expense.title,
-              style: Theme.of(context).textTheme.titleLarge,
+            Row(
+              children: [
+                Icon(categoryIcons[expense.category]),
+                const SizedBox(width: 8),
+                Text(
+                  expense.title,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ],
             ),
             const SizedBox(height: 4),
             Row(
               children: [
-                Text('\$ ${expense.amount.toStringAsFixed(2)}'),
-                const Spacer(),
                 Row(children: [
                   Text(expense.formattedDate),
-                  const SizedBox(width: 8),
-                  Icon(categoryIcons[expense.category]),
+                  // const SizedBox(width: 8),
+                  // Icon(categoryIcons[expense.category]),
                 ]),
+                const Spacer(),
+                Text('\$ ${expense.amount.toStringAsFixed(2)}'),
               ],
             )
           ],
